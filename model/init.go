27cfa95987324aac3b66c5c2367d8973904c8623
madwolfcrazy <madwolfcrazy@gmail.com>
2021-09-04 21:13:20 +0800

    fix:update gin to 1.7.3 && update gorm from v1 to v2

--- model/init.go
+++ model/init.go
@@ -3,10 +3,9 @@ package model
 import (
 	"time"
 
-	"github.com/jinzhu/gorm"
-
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	//
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 // DB connection single unit
@@ -14,20 +13,23 @@ var DB *gorm.DB
 
 // InitDatabase init function
 func InitDatabase(connString string) {
-	db, err := gorm.Open("mysql", connString)
+	db, err := gorm.Open(mysql.Open(connString), &gorm.Config{})
 	// 开发调试模式可打开查看 gorm 日志
-	//db.LogMode(true)
 	// Error
 	if err != nil {
 		panic(err)
 	}
 	//设置连接池
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
 	//空闲
-	db.DB().SetMaxIdleConns(10)
+	sqlDB.SetMaxIdleConns(10)
 	//打开
-	db.DB().SetMaxOpenConns(100)
+	sqlDB.SetMaxOpenConns(100)
 	//超时
-	db.DB().SetConnMaxLifetime(time.Minute * 30)
+	sqlDB.SetConnMaxLifetime(time.Minute * 30)
 	// 设置自动建立表时的字符集
 	db = db.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4 auto_increment=1")
 	DB = db
